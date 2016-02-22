dca38e1dec3777891eb120760e82885e567cdb54
Leonardo <lvbernal@gmail.com>
2016-02-22 18:34:53 -0500

    style files added to package.json

--- design/package.js
+++ design/package.js
@@ -2,23 +2,38 @@ Package.describe({
   name: 'themidgame:design',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Design package for meteor applications',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/themidgame/tmg-meteor-design.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
-Package.onUse(function(api) {
+Package.onUse(function (api) {
   api.versionsFrom('1.2.1');
   api.use('ecmascript');
-  api.addFiles('design.js');
+
+  api.use('fourseven:scss', 'client');
+  api.use('reywood:bootstrap3-sass', 'client');
+  api.addFiles(getFilesForClient(), 'client');
 });
 
-Package.onTest(function(api) {
+Package.onTest(function (api) {
   api.use('ecmascript');
   api.use('tinytest');
   api.use('themidgame:design');
-  api.addFiles('design-tests.js');
 });
+
+function getFilesForClient() {
+  return [
+    'stylesheets/main.scss',
+    'stylesheets/bootstrap/_dropdown.scss',
+    'stylesheets/bootstrap/_type.scss',
+    'stylesheets/bootstrap/_variables.scss',
+    'stylesheets/utils/_globals.scss',
+    'stylesheets/utils/_icons.scss',
+    'stylesheets/utils/_mixins.scss',
+    'stylesheets/utils/_variables.scss',
+  ];
+};
